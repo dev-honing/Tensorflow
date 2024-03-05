e537c6b0e3e0cabb3d6ab7f2bf0c824e37c47e92
Ho Nyeong <dev.honing@gmail.com>
2024-03-05 13:05:36 +0900

    Feat: 데이터 세트를 섞어 배치 만들기

--- main.py
+++ main.py
@@ -45,5 +45,14 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = x_train[..., tf.newaxis].astype("float32")
 x_test = x_test[..., tf.newaxis].astype("float32")
 
-print(x_train.shape) # (60000, 28, 28, 1)
-print(x_test.shape) # (10000, 28, 28, 1)
+# 배열의 모양 확인을 위한 콘솔 출력
+# print(x_train.shape) # (60000, 28, 28, 1)
+# print(x_test.shape) # (10000, 28, 28, 1)
+
+# 데이터 세트 생성(.shuffle 메서드로 섞기)
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+# 섞인 데이터 세트 객체 확인을 위한 콘솔 출력(여기서 None은 데이터 세트의 배치 크기가 유동적으로 조정됨을 의미)
+print(train_ds) 
+print(test_ds) 
